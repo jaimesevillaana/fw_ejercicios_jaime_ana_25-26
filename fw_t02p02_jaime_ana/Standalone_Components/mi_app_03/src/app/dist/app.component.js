322e12b79b3a5ddf2f91f80ef62ecce4605a920d
Ana Jaime <161146852+jaimesevillaana@users.noreply.github.com>
2026-02-02 09:22:46 +0100

    Modificando el componente Home y creando componente housing-location

--- fw_t02p02_jaime_ana/Standalone_Components/mi_app_03/src/app/dist/app.component.js
+++ fw_t02p02_jaime_ana/Standalone_Components/mi_app_03/src/app/dist/app.component.js
@@ -9,6 +9,7 @@ exports.__esModule = true;
 exports.AppComponent = void 0;
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
+var home_component_1 = require("./home/home.component");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
         this.title = core_1.signal('mi_app_03');
@@ -16,7 +17,7 @@ var AppComponent = /** @class */ (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'app-root',
-            imports: [router_1.RouterOutlet],
+            imports: [router_1.RouterOutlet, home_component_1.HomeComponent],
             templateUrl: './app.component.html',
             styleUrl: './app.component.css'
         })
