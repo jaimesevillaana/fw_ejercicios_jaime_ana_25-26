23b4bdd46bd55d111ce2115b56530f6e4c5bba03
Ana Jaime <161146852+jaimesevillaana@users.noreply.github.com>
2026-02-02 10:02:51 +0100

    input

--- fw_t02p02_jaime_ana/Standalone_Components/mi_app_03/src/app/housing-location/housing-location.component.ts
+++ fw_t02p02_jaime_ana/Standalone_Components/mi_app_03/src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { HousinglocationInfo } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  housingLocation = input.required<HousinglocationInfo>();
 }
